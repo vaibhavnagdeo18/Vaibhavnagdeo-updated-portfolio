d871a5079bf8932de7b4536b021a75642ee81b1e
Vaibhav Nagdeo <vaibhavnagdeo@gmail.com>
2026-03-19 18:52:37 +0530

    chore: migrate from gsap-trial to standard gsap package

--- src/gsap-plugins.d.ts
+++ src/gsap-plugins.d.ts
@@ -1,4 +1,4 @@
-declare module "gsap-trial/SplitText" {
+declare module "gsap/SplitText" {
   export class SplitText {
     constructor(target: string | Element | (string | Element)[] | NodeList, vars: object);
     chars: HTMLElement[];
@@ -8,7 +8,7 @@ declare module "gsap-trial/SplitText" {
   }
 }
 
-declare module "gsap-trial/ScrollSmoother" {
+declare module "gsap/ScrollSmoother" {
   export class ScrollSmoother {
     static create(vars: object): ScrollSmoother;
     static refresh(soft?: boolean): void;
@@ -18,7 +18,7 @@ declare module "gsap-trial/ScrollSmoother" {
   }
 }
 
-declare module "gsap-trial/all" {
-  export * from "gsap-trial/SplitText";
-  export * from "gsap-trial/ScrollSmoother";
+declare module "gsap/all" {
+  export * from "gsap/SplitText";
+  export * from "gsap/ScrollSmoother";
 }
